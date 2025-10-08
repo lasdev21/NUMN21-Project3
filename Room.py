ceb9c2a07adc08db6eb7ec8bc626aaf0a6b98453
CHMN-LU <co2441ha-s@student.lu.se>
2025-10-08 22:58:34 +0200

    Started code that allows adding of rooms to the apartment. Can specify where they go and they will be added to the floor plan.

--- Room.py
+++ Room.py
@@ -1,11 +1,26 @@
+import numpy as np
 # Class which holds information pertaining to each room in the simulation
 
 class Room():
     # Room needs to know it's dimensions for temperature matrix
     # it needs to know it's boundaries for sparse matrix construction and solving
     # needs to be given the boundaries from the apartment class
-    def __init__(self, rank, dim_array, scale, delta_x):
-        self.rank = rank
+    def __init__(self, process_rank, dim_array, delta_x):
+        """
+        Give the room the rank of the process it's running on and it's size.
+        NOTE: dim_array should be (width, height)
+        """
+        self.rank = process_rank
+        self.dim = dim_array
+        self.scale = int(1/delta_x)
+        # Create a temperature array for this room
+        self.u = np.zeros((dim_array / delta_x).astype(np.int16))
+    def get_dims(self):
+        return self.dim
+    def get_scale(self):
+        return self.scale
+    def get_temp_array(self):
+        return self.u
     
     def add_boundaries(self, boundary_array):
         # Take array of form [[room2,'D', 'top'], [room3, 'N', 'left']] where room2, etc.. are Room objs
