113fa3abdbd6d693a72f270dd0d96e97a023f969
CHMN-LU <co2441ha-s@student.lu.se>
2025-10-16 12:13:08 +0200

    Added argparse for running program, takes layout, delta x, iterations, omega, and whether to plot or not

--- Apartment.py
+++ Apartment.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from Room import Room
 from mpi4py import MPI
+import argparse
+import os, sys
 
 class Apartment():
     # Know about its rooms, how many, how they are connected
@@ -339,7 +341,7 @@ class Apartment():
         plt.colorbar(sm, ax=ax, label='Value')
 
         plt.tight_layout()
-        plt.title(fr"Heat distribution with $\Delta x={delta_x}$ after {iterations} iterations")
+        plt.title(fr"Heat distribution with $\Delta x=1/{int(1/delta_x)}$ after {iterations} iterations")
         plt.show()
     
     # Repr output
@@ -358,54 +360,80 @@ class Apartment():
 
 if __name__ == '__main__':
     # create apartment if on rank 0 process
-    # Create the rooms first, telling each one its size
-    # Apartment holds array of rooms and x,y positions of all of them
-    #room1 = Room(size m, n)
-    #room2 = Room(size x, y)
-    #room1.create_boundaries(array of boundaries for room 1)
-    # Then arrange the rooms into an apartment
+    # The apartment creates the room layout and
+    # holds array of rooms and temperatures of all of them
     
     commMain = MPI.Comm.Clone(MPI.COMM_WORLD)
     rank = commMain.Get_rank()
-    delta_x = 1/10
-    layout = 'project3'
     
-    iterations = 25
-    omega = 0.8
+    # Temporarily remove printing from all but master process to stop multiple help/error strings from argparse
+    if rank != 0:
+        sys.stdout = open(os.devnull, 'w')
+        sys.stderr = open(os.devnull, 'w')
+    
+    # Argparse
+    # Accepts arguments for layout, delta_x, iterations, omega
+    parser = argparse.ArgumentParser(description="Modelling Laplace heat equation on room structure using parallel MPI processes")
+    parser.add_argument("layout", choices=['project3', 'project3a'], 
+                        help="""Specify the room layout you want to solve on.""")
+    parser.add_argument("-dx", "--delta_x", type=int, default=10, 
+                        help="The reciprocal of the gridwidth to choose. I.e. a value of 10 means 1/10 delta x.")
+    parser.add_argument("-it", "--iterations", type=int, default=10,
+                        help="The number of iterations to perform.")
+    parser.add_argument("-w", "--omega", type=float, default=0.8,
+                        help="The relaxation parameter omega.")
+    parser.add_argument("-p", "--plot", type=int, choices=[0, 1], default=1,
+                        help="Boolean value for determining plotting of outputs. Defaults to 1 (True). Note that for large (~100) delta x, plotting is very slow.")
+    args = parser.parse_args()
+    
+    delta_x = 1/args.delta_x
+    layout = args.layout
+    iterations = args.iterations
+    omega = args.omega
+    make_plot = args.plot
+    
+    # Restore stdout and stderr
+    if rank != 0:
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+        
+    #print(f"On process {rank}")
+    #print(f"Got args:\nlayout:{layout}\ndelta:{delta_x}\niters:{iterations}\nomega:{omega}")
+    # Begin program operation
+    # Project3 base
     if layout == 'project3':
         if rank== 0:
             apartment = Apartment(commMain)
             apartment.initialize_apartment_proj3(delta_x)
             apartment.solve(iterations, omega)
-            # SOME PLOTTING STUFF
+            # Update the floor plan with final temps
             apartment.update_floor_plan()
             # Plot the heat values
-            apartment.plot_heatmap(delta_x)
-            
-        if rank == 1:
+            if make_plot:
+                apartment.plot_heatmap(delta_x)
+            else:
+                print("Completed process, plotting disabled")
+        elif rank == 1:
             room1 = Room(rank, np.array([1, 1]), delta_x, commMain)
             #let this work
             #commMain.Recv(room1.A, source=0, tag=0)
             room1.A = room1.recv_sparse_matrix(source=0, shape_tag=0, data_tag=1)
             #print(f"current room A {rank}: \n{room1.A.toarray()}")
             room1.solve(iterations, omega)
-        if rank == 2:
+        elif rank == 2:
             room2 = Room(rank, np.array([1, 2]), delta_x, commMain)
             #commMain.Recv(room2.A, source=0, tag=1)
             room2.A = room2.recv_sparse_matrix(source=0, shape_tag=2, data_tag=3)
             #print(f"current room A {rank}: \n{room2.A.toarray()}")
             room2.solve(iterations, omega)
-        if rank == 3:
+        elif rank == 3:
             room3 = Room(rank, np.array([1, 1]), delta_x, commMain)
             #commMain.Recv(room3.A, source=0, tag=2)
             room3.A = room3.recv_sparse_matrix(source=0, shape_tag=4, data_tag=5)
             #print(f"current room A {rank}: \n{room3.A.toarray()}")
             room3.solve(iterations, omega)
-        else:
-            pass
-        #print(apartment)
+    # Extension
     elif layout == 'project3a':
-        #omega = 0.8
         if rank== 0:
             apartment = Apartment(commMain)
             apartment.initialize_apartment_proj3a(delta_x)
@@ -413,7 +441,10 @@ if __name__ == '__main__':
             # SOME PLOTTING STUFF
             apartment.update_floor_plan()
             # Plot the heat values
-            apartment.plot_heatmap(delta_x)
+            if make_plot:
+                apartment.plot_heatmap(delta_x)
+            else:
+                print("Completed process, plotting disabled")
         elif rank == 1:
             room1 = Room(rank, np.array([1, 1])*2, delta_x, commMain)
             #let this work
